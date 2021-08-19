7511296408f7c3fd8fcf18da8d79d4bd4095c9db
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-08-18 22:36:04 -0700

    Revert "added a new contact component to troubleshoot netlify forms"
    This reverts commit 50ef5e1ac6c997c15192395aa46e300632327ce6.

--- components/contact.js
+++ components/contact.js
@@ -2,24 +2,67 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Contact() {
+
   return (
-    <form className="bg-gray-300" name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label htmlFor="yourname" />
-        <input type="text" name="name" id="yourname" />
-      </p>
-      <p>
-        <label htmlFor="youremail" />
-        <input type="email" name="email" id="youremail" />
-      </p>
-      <p>
-        <label htmlFor="yourmessage" />
-        <textarea name="message" id="yourmessage"></textarea>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
+    <div className="bg-gray-200 flex flex-col items-center justify-center m-6">
+      <p className="pt-2"> Contact us</p>
+      <form
+        className=" w-2/3 xl:w-1/3"
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="lname"
+      >
+        <div class="m-2 hidden">
+        <input
+            className="w-full"
+            type="text"
+            name="lname"
+            placeholder="Your Name"
+          />
+        </div>
+
+        <div className="m-2">
+          <input
+            className="w-full"
+            type="text"
+            name="fname"
+            placeholder="Your Name"
+          />
+        </div>
+
+        <div className="m-2">
+          <input
+            className="w-full"
+            type="tel"
+            name="tel"
+            placeholder="Your Phone Number"
+          />
+        </div>
+
+        <div className="m-2">
+          <input
+            className="w-full"
+            type="email"
+            name="email"
+            placeholder="Your Email"
+          />
+        </div>
+
+        <div className="m-2">
+          <textarea
+            className="w-full"
+            name="message"
+            placeholder="Your Message"
+          ></textarea>
+        </div>
+
+        <div className="m-2 bg-blue-500">
+          <button className="w-full" type="submit">
+            Send
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
