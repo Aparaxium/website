9d0d6b465d899262dda5f71f3e47ec864a8fa73e
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-08-18 22:42:57 -0700

    update contact, adding labels for troubleshooting netlify forms

--- components/contact.js
+++ components/contact.js
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Contact() {
-
   return (
     <div className="bg-gray-200 flex flex-col items-center justify-center m-6">
       <p className="pt-2"> Contact us</p>
@@ -11,10 +10,10 @@ export default function Contact() {
         name="contact"
         method="POST"
         data-netlify="true"
-        netlify-honeypot="lname"
+        //netlify-honeypot="lname"
       >
         <div class="m-2 hidden">
-        <input
+          <input
             className="w-full"
             type="text"
             name="lname"
@@ -23,36 +22,44 @@ export default function Contact() {
         </div>
 
         <div className="m-2">
+          <label htmlFor="yourname" />
           <input
             className="w-full"
             type="text"
-            name="fname"
+            name="name"
+            id="yourname"
             placeholder="Your Name"
           />
         </div>
 
         <div className="m-2">
+          <label htmlFor="yourphone" />
           <input
             className="w-full"
             type="tel"
             name="tel"
+            id="yourphone"
             placeholder="Your Phone Number"
           />
         </div>
 
         <div className="m-2">
+          <label htmlFor="youremail" />
           <input
             className="w-full"
             type="email"
             name="email"
+            id="youremail"
             placeholder="Your Email"
           />
         </div>
 
         <div className="m-2">
+        <label htmlFor="yourmessage"/>
           <textarea
             className="w-full"
             name="message"
+            id="yourmessage"
             placeholder="Your Message"
           ></textarea>
         </div>
