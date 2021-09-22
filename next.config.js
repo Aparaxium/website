cae0d3f81716ab4af1877464a09901e04432c9c4
aparaxium <10635434+Aparaxium@users.noreply.github.com>
2021-09-22 00:46:08 -0700

    Use Preact instead of React when building

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@
 //nextjs build analyzer
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 });
 
 module.exports = withBundleAnalyzer({ reactStrictMode: true });
--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -3,7 +3,7 @@ import { useTheme } from "next-themes";
 import path from "path";
 import { ReactElement, useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import {
   vs,
   vscDarkPlus,
